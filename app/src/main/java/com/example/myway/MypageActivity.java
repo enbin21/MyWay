a9a8d302c3fe19da7c9804b5a6a3734374e3eb6d
enbin21 <enbin14@gmail.com>
2022-07-15 23:31:57 +0900

    화면 연결

--- app/src/main/java/com/example/myway/MypageActivity.java
+++ app/src/main/java/com/example/myway/MypageActivity.java
@@ -1,10 +1,14 @@
 package com.example.myway;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +28,42 @@ public class MypageActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_left);
         getSupportActionBar().setTitle("");
         toolbar.setSubtitle("");
+
+        TextView profiledit = (TextView) findViewById(R.id.profile_edit);
+        profiledit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileEditActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView pwdedit = (TextView) findViewById(R.id.pwd_edit);
+        pwdedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PwdEditActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView deleteaccount = (TextView) findViewById(R.id.delete_account);
+        deleteaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DeleteAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView notice = (TextView) findViewById(R.id.notice);
+        notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NoticeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
